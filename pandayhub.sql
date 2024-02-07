5aa830e173572200ef35b6a283008695c7a20129
ShiroKuroGeo <inocgeorgealfeser@gmail.com>
2024-02-07 17:11:23 +0800

    Ready to Check - 2.7

--- pandayhub.sql
+++ pandayhub.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2023 at 01:42 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Feb 07, 2024 at 10:10 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,28 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `applicants` (
   `appli_id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
   `poser_id` int(11) NOT NULL,
   `appliUser_id` int(11) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `applicants`
 --
 
-INSERT INTO `applicants` (`appli_id`, `poser_id`, `appliUser_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 22, 2, '2023-12-20 01:24:19', '2023-12-20 01:24:19'),
-(2, 2, 22, 1, '2023-12-20 05:08:13', '2023-12-20 05:08:13'),
-(3, 2, 22, 1, '2023-12-20 05:08:23', '2023-12-20 05:08:23'),
-(4, 2, 23, 1, '2023-12-21 08:58:07', '2023-12-21 08:58:07'),
-(5, 2, 23, 1, '2023-12-21 08:58:15', '2023-12-21 08:58:15'),
-(6, 2, 23, 1, '2023-12-21 08:58:49', '2023-12-21 08:58:49'),
-(7, 2, 23, 1, '2023-12-21 08:59:00', '2023-12-21 08:59:00'),
-(8, 2, 23, 1, '2023-12-21 09:14:49', '2023-12-21 09:14:49'),
-(9, 2, 23, 1, '2023-12-21 09:15:55', '2023-12-21 09:15:55'),
-(10, 2, 23, 1, '2023-12-21 09:15:58', '2023-12-21 09:15:58');
+INSERT INTO `applicants` (`appli_id`, `job_id`, `poser_id`, `appliUser_id`, `status`, `created_at`, `updated_at`) VALUES
+(52, 4, 46, 45, 1, '2024-02-07 06:42:29', '2024-02-07 06:42:29');
 
 -- --------------------------------------------------------
 
@@ -64,37 +56,7 @@ CREATE TABLE `chats` (
   `reciever` int(11) NOT NULL,
   `message` text NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `chats`
---
-
-INSERT INTO `chats` (`chatId`, `sender`, `reciever`, `message`, `created`) VALUES
-(1, 1, 22, '123123', '2023-12-20 06:59:51'),
-(2, 2, 22, 'H', '2023-12-20 06:10:35'),
-(3, 22, 2, 'QWE', '2023-12-20 05:47:41'),
-(4, 22, 2, 'hello world', '2023-12-20 05:52:27'),
-(5, 22, 2, 'hi\r\n', '2023-12-20 05:53:04'),
-(6, 22, 2, 'hello', '2023-12-20 05:53:09'),
-(7, 22, 2, 'qwe', '2023-12-20 05:54:03'),
-(8, 22, 2, 'oy', '2023-12-20 05:55:15'),
-(9, 22, 2, 'oi', '2023-12-20 05:55:32'),
-(10, 22, 2, 'qwe', '2023-12-20 05:58:04'),
-(11, 22, 2, 'oi boang', '2023-12-20 06:02:22'),
-(12, 22, 2, '144778825454', '2023-12-20 06:02:57'),
-(13, 22, 2, '123123', '2023-12-20 06:04:39'),
-(14, 22, 2, 'Hello world', '2023-12-20 06:15:56'),
-(15, 2, 22, 'oh unsa man', '2023-12-20 06:16:32'),
-(16, 22, 2, '154', '2023-12-20 06:17:47'),
-(17, 2, 22, 'ILO', '2023-12-20 06:18:10'),
-(18, 22, 2, 'qwe', '2023-12-20 06:48:57'),
-(19, 22, 1, 'unsa man?', '2023-12-20 07:00:00'),
-(20, 22, 1, 'vxcvxcv', '2023-12-20 12:28:59'),
-(21, 23, 1, 'jhjhjk', '2023-12-20 15:52:20'),
-(22, 23, 2, 'jhkjh', '2023-12-20 15:52:28'),
-(23, 23, 1, 'l\r\n', '2023-12-20 15:52:37'),
-(24, 23, 1, 'kjhkjh\r\n', '2023-12-20 15:53:12');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -109,7 +71,27 @@ CREATE TABLE `contracts` (
   `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formapplication`
+--
+
+CREATE TABLE `formapplication` (
+  `form_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `applicant_id` int(11) NOT NULL,
+  `job_id` int(11) NOT NULL,
+  `date_agreement_start` text NOT NULL,
+  `date_agreement_end` text NOT NULL,
+  `helperNumber` int(11) NOT NULL,
+  `helpers_paid` int(11) NOT NULL,
+  `form_status` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -124,18 +106,14 @@ CREATE TABLE `hireds` (
   `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `hireds`
 --
 
 INSERT INTO `hireds` (`hired_id`, `user_id`, `user_hired`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, 1, '2023-12-19 17:17:20', '2023-12-19 17:17:20'),
-(4, 2, 2, 1, '2023-12-19 17:19:08', '2023-12-19 17:19:08'),
-(7, 2, 22, 1, '2023-12-20 01:38:34', '2023-12-20 01:38:34'),
-(12, 2, 22, 1, '2023-12-20 01:48:59', '2023-12-20 01:48:59'),
-(13, 2, 22, 1, '2023-12-20 01:50:22', '2023-12-20 01:50:22');
+(78, 47, 45, 10, '2024-02-07 06:42:30', '2024-02-07 06:42:30');
 
 -- --------------------------------------------------------
 
@@ -156,18 +134,22 @@ CREATE TABLE `jobs` (
   `job_status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `jobs`
 --
 
 INSERT INTO `jobs` (`job_id`, `job_poser`, `picture`, `job_title`, `job_project`, `job_location`, `job_require_exp`, `projectType`, `job_payment`, `job_status`, `created_at`, `updated_at`) VALUES
-(1, 2, 'roofing.jpg', 'Flooring', 'Flooring', 'Poblacion', '2 years', 'Daily', '12222', 1, '2023-12-19 16:43:41', '2023-12-19 16:43:41'),
-(3, 2, '173472113_508180986867522_5999261301032980265_n.jpg', 'Mason', 'Daily', 'Camolinas', '12 years exp in masoning', '', '1122', 1, '2023-12-19 18:01:18', '2023-12-19 18:01:18'),
-(4, 2, '311202075_1109981239642694_4928069177595602249_n.jpg', 'House building', 'Pakyaw', 'Basak', 'Tig hilot sa akong balay.', '', '12311', 1, '2023-12-19 18:08:11', '2023-12-19 18:08:11'),
-(5, 2, 'kibot.jpg', 'Roofing', 'Pakyaw', 'Cordova', 'Tig hilot sa among atop.', '', '5678', 1, '2023-12-19 18:09:42', '2023-12-19 18:09:42'),
-(6, 23, '', 'tiles', 'Daily', 'cogon', '4years', '', '15000', 1, '2023-12-21 09:42:09', '2023-12-21 09:42:09');
+(1, 45, 'roofing.jpg', 'Flooring', 'Flooring', 'Poblacion', '2 years', 'Daily', '12222', 1, '2023-12-19 16:43:41', '2023-12-19 16:43:41'),
+(3, 45, '173472113_508180986867522_5999261301032980265_n.jpg', 'Mason', 'Daily', 'Camolinas', '12 years exp in masoning', 'Daily', '1122', 1, '2023-12-19 18:01:18', '2023-12-19 18:01:18'),
+(4, 46, '311202075_1109981239642694_4928069177595602249_n.jpg', 'House building', 'Pakyaw', 'Basak', 'Tig hilot sa akong balay.', 'Daily', '12311', 1, '2023-12-19 18:08:11', '2023-12-19 18:08:11'),
+(7, 47, '406810112_302821768801035_3765542810081729389_n.jpg', 'mason', 'Daily', 'skina japan', '2 years of existence', 'Daily', '150', 1, '2023-12-22 14:11:28', '2023-12-22 14:11:28'),
+(8, 49, '406810112_302821768801035_3765542810081729389_n.jpg', 'mayor', 'Daily', 'cordova', 'mayoring', 'Daily', '80000', 1, '2023-12-22 16:15:05', '2023-12-22 16:15:05'),
+(9, 45, 'dance.jpg', 'dancing', 'Pakyaw', 'poblacion', 'dancing chiou', 'Daily', '12311', 1, '2023-12-22 16:16:03', '2023-12-22 16:16:03'),
+(10, 46, '123.jpg', 'panday guba', 'Daily', 'cordova', 'no experience', 'Daily', '50', 1, '2023-12-22 20:52:29', '2023-12-22 20:52:29'),
+(11, 48, 'arrrrrr.webp', 'Finishing', 'Pakyaw', 'Davao', 'at least 2 years in Masonry', 'Daily', '400,000', 1, '2023-12-23 02:11:09', '2023-12-23 02:11:09'),
+(13, 47, 'wiselogo.png', '123`123`', 'Daily', '123', '321132123', '', '132', 1, '2024-02-07 05:22:51', '2024-02-07 05:22:51');
 
 -- --------------------------------------------------------
 
@@ -181,19 +163,19 @@ CREATE TABLE `panday` (
   `Panday_location` varchar(125) NOT NULL,
   `Panday_skill` varchar(125) NOT NULL,
   `Panday_level` varchar(125) NOT NULL,
+  `panday_exp` varchar(125) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `panday`
 --
 
-INSERT INTO `panday` (`id`, `user_id`, `Panday_location`, `Panday_skill`, `Panday_level`, `status`, `created_at`, `update_at`) VALUES
-(1, 22, 'Poblacion', 'Panday', '31 years', 2, '2023-12-19 17:14:08', '2023-12-19 17:14:08'),
-(2, 1, 'Cordova', 'Pinishing', '3 years', 1, '2023-12-19 17:14:08', '2023-12-19 17:14:08'),
-(3, 2, 'Lapu-Lapu', 'Mason', 'Daily', 1, '2023-12-19 18:06:20', '2023-12-19 18:06:20');
+INSERT INTO `panday` (`id`, `user_id`, `Panday_location`, `Panday_skill`, `Panday_level`, `panday_exp`, `status`, `created_at`, `update_at`) VALUES
+(11, 45, 'Poblacion', 'Mason', 'Daily', '2 years in masonry.', 2, '2023-12-26 13:20:23', '2023-12-26 13:20:23'),
+(12, 48, '132123123', '123132132', 'Pakyaw', '123213123', 2, '2024-02-07 05:23:45', '2024-02-07 05:23:45');
 
 -- --------------------------------------------------------
 
@@ -208,7 +190,15 @@ CREATE TABLE `reports` (
   `reason` text NOT NULL,
   `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `user_id`, `reported_id`, `reason`, `create_at`, `updated_at`) VALUES
+(3, 33, 33, 'amaw amaw', '2023-12-22 20:59:30', '2023-12-22 20:59:30'),
+(5, 47, 45, 'Ambot lang trip nako boot ka?', '2024-02-07 09:08:43', '2024-02-07 09:08:43');
 
 -- --------------------------------------------------------
 
@@ -225,21 +215,26 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `phoneNumber` int(11) NOT NULL,
   `phoneNumber2` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `no_of_rating` int(11) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1,
   `role` int(11) NOT NULL DEFAULT 1,
   `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userId`, `profile`, `firstname`, `lastname`, `email`, `password`, `phoneNumber`, `phoneNumber2`, `status`, `role`, `create_at`, `updated_at`) VALUES
-(1, 'roofing.jpg', 'jovet', 'quillan', 'jovetfirst@gmail.com', '202cb962ac59075b964b07152d234b70', 0, 0, 1, 2, '2023-09-05 09:17:08', '2023-09-05 09:17:08'),
-(2, 'qq.jpg', 'Jovet', 'Quillian', 'jovet@gmail.com', '202cb962ac59075b964b07152d234b70', 2147483647, 2147483647, 1, 1, '2023-09-05 09:30:24', '2023-09-05 09:30:24'),
-(22, 'profile.jpg', 'Jovetshi', 'kie', 'jovetshi@gmail.com', '202cb962ac59075b964b07152d234b70', 1231123123, 123123123, 1, 1, '2023-12-20 01:22:52', '2023-12-20 01:22:52'),
-(23, 'profile.jpg', 'jovet', 'quillan', 'jovet@gmail.com', '0c9f3c8a9564dceff87abc5a8fdd2d43', 0, 0, 1, 1, '2023-12-20 15:50:25', '2023-12-20 15:50:25');
+INSERT INTO `users` (`userId`, `profile`, `firstname`, `lastname`, `email`, `password`, `phoneNumber`, `phoneNumber2`, `rating`, `no_of_rating`, `status`, `role`, `create_at`, `updated_at`) VALUES
+(45, 'logo.png', 'jovet', 'first', 'jovetfirst@gmail.com', '202cb962ac59075b964b07152d234b70', 0, 0, 3, 1, 1, 1, '2023-12-26 08:18:25', '2023-12-26 08:18:25'),
+(46, 'profile.jpg', 'jovet', '1st', 'jovet1st@gmail.com', '202cb962ac59075b964b07152d234b70', 0, 0, 0, 0, 1, 2, '2023-12-26 08:18:49', '2023-12-26 08:18:49'),
+(47, 'profile.jpg', '123', '123', '123@123', '202cb962ac59075b964b07152d234b70', 0, 0, 0, 0, 1, 3, '2023-12-26 08:24:55', '2023-12-26 08:24:55'),
+(48, 'profile.jpg', '123123', '123', 'cliyent@gmail.com', '202cb962ac59075b964b07152d234b70', 0, 0, 0, 0, 1, 1, '2023-12-26 08:26:20', '2023-12-26 08:26:20'),
+(49, 'profile.jpg', 'ewq', 'ewq', 'qewq@12123', 'efe6398127928f1b2e9ef3207fb82663', 0, 0, 0, 0, 1, 3, '2023-12-26 08:26:53', '2023-12-26 08:26:53'),
+(50, 'profile.jpg', 'jovet', 'quillan', 'jovet123@', '912ec803b2ce49e4a541068d495ab570', 0, 0, 0, 0, 1, 3, '2024-02-01 03:55:17', '2024-02-01 03:55:17'),
+(51, 'profile.jpg', '123', '123', '123@123123', '202cb962ac59075b964b07152d234b70', 0, 0, 0, 0, 1, 3, '2024-02-04 07:51:01', '2024-02-04 07:51:01');
 
 --
 -- Indexes for dumped tables
@@ -262,6 +257,12 @@ ALTER TABLE `chats`
 --
 ALTER TABLE `contracts`
   ADD PRIMARY KEY (`contracs_Id`);
+
+--
+-- Indexes for table `formapplication`
+--
+ALTER TABLE `formapplication`
+  ADD PRIMARY KEY (`form_id`);
 
 --
 -- Indexes for table `hireds`
@@ -301,13 +302,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `applicants`
 --
 ALTER TABLE `applicants`
-  MODIFY `appli_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `appli_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `contracts`
@@ -316,34 +317,40 @@ ALTER TABLE `contracts`
   MODIFY `contracs_Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `formapplication`
+--
+ALTER TABLE `formapplication`
+  MODIFY `form_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `hireds`
 --
 ALTER TABLE `hireds`
-  MODIFY `hired_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `hired_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `panday`
 --
 ALTER TABLE `panday`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
